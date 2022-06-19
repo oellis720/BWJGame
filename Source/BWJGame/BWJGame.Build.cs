12ea86c5dd2ca3bad97eb61a75b71be21ed1e8a7
oellis720 <oellis720@gmail.com>
2022-06-19 12:09:29 -0400

    Update BWJGame.Build.cs
    Allows engine to use Paper2D

--- Source/BWJGame/BWJGame.Build.cs
+++ Source/BWJGame/BWJGame.Build.cs
@@ -8,7 +8,7 @@ public class BWJGame : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Paper 2D" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
